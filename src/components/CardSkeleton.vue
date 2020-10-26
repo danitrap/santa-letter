5e3487c4cacb269b036f89d364dff1fac63760d4
Daniele Trapani <daniele.trapani@outlook.it>
2020-10-26 23:50:04 +0100

    feature: add loading skeletons

---
+++ src/components/CardSkeleton.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="skeleton skeleton--title"></div>
+      <div class="skeleton skeleton--title"></div>
+      <div class="skeleton skeleton--title"></div>
+      <div class="skeleton skeleton--subtitle"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "CardSkeleton",
+  });
+</script>
+
+<style lang="scss" scoped>
+  .skeleton {
+    width: 100%;
+    background: #ccc;
+    border-radius: 4px;
+    position: relative;
+    overflow: hidden;
+
+    &--title {
+      height: 16px;
+      margin-bottom: 16px;
+    }
+
+    &--subtitle {
+      height: 12px;
+      margin-bottom: 12px;
+      width: 50%;
+    }
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      left: -150px;
+      top: 0;
+      height: 100%;
+      width: 150px;
+      background: linear-gradient(
+        to right,
+        transparent 0%,
+        #e8e8e8 50%,
+        transparent 100%
+      );
+      animation: load 1.5s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+    }
+  }
+
+  @keyframes load {
+    from {
+      left: -150px;
+    }
+    to {
+      left: 100%;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,19 @@
           <letters-list />
         </template>
         <template #fallback>
-          Loading...
+          <div
+            class="columns"
+            v-for="(n, i) in Array.from(new Array(3))"
+            :key="i"
+          >
+            <div
+              class="column is-4"
+              v-for="(n, i) in Array.from(new Array(3))"
+              :key="i"
+            >
+              <card-skeleton />
+            </div>
+          </div>
         </template>
       </Suspense>
     </div>
@@ -21,12 +33,14 @@
 
   import LettersList from "../components/LettersList.vue";
   import AddButton from "../components/AddButton.vue";
+  import CardSkeleton from "../components/CardSkeleton.vue";
 
   export default defineComponent({
     name: "Home",
     components: {
       LettersList,
       AddButton,
+      CardSkeleton,
     },
   });
 </script>
