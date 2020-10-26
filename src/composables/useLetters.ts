f4c47d7b42d87a486bc94fc2e174891c721df8f2
Daniele Trapani <daniele.trapani@outlook.it>
2020-10-26 23:21:55 +0100

    refactor: clean up compsable

--- src/composables/useLetters.ts
+++ src/composables/useLetters.ts
@@ -1,21 +1,13 @@
 import { computed, ref } from "vue";
 import { ILetter } from "@/interfaces/shared";
 import { chunk } from "@/helpers/chunk";
-import { db } from "../firebaseDatabase";
 
-import { like, unlike } from "../services/letterService";
-
-const letters = ref<ILetter[]>([]);
+import { like, unlike, watchCollection } from "../services/letterService";
 
 export const useLetters = async () => {
-  db.collection("letters")
-    .orderBy("createdAt", "desc")
-    .onSnapshot((snapshot) => {
-      const newLetters = snapshot.docs.map(
-        (doc) => ({ ...doc.data(), id: doc.id } as ILetter)
-      );
-      letters.value = newLetters;
-    });
+  const letters = ref<ILetter[]>([]);
+
+  await watchCollection((newLetters) => (letters.value = newLetters));
 
   const lettersInColumns = computed(() => chunk<ILetter>(letters.value, 3));
 
--- src/services/letterService.ts
+++ src/services/letterService.ts
@@ -2,7 +2,26 @@ import * as firebase from "firebase";
 import { db } from "../firebaseDatabase";
 import $store from "../store";
 
-import { ILetterSendPayload } from "../interfaces/shared";
+import { ILetter, ILetterSendPayload } from "../interfaces/shared";
+
+export const watchCollection = (effect: (letters: ILetter[]) => void) => {
+  return new Promise((resolve) => {
+    let firstRun = true;
+
+    db.collection("letters")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        effect(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as ILetter))
+        );
+
+        if (firstRun) {
+          firstRun = false;
+          resolve();
+        }
+      });
+  });
+};
 
 export const like = (letterId: string) => {
   const letterRef = db.collection("letters").doc(letterId);
